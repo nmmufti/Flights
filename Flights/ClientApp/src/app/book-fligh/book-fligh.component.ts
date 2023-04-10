c037233a22afb1c54faa661ea971848227002883
nmmufti <80278616+nmmufti@users.noreply.github.com>
2023-04-10 14:15:27 +0100

    Day 8

--- Flights/ClientApp/src/app/book-fligh/book-fligh.component.ts
+++ Flights/ClientApp/src/app/book-fligh/book-fligh.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute} from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { FlightService } from './../api/services/flight.service';
 import { FlightRm } from '../api/models';
 
@@ -11,7 +11,8 @@ import { FlightRm } from '../api/models';
 
 export class BookFlighComponent implements OnInit {
 
- constructor(private route: ActivatedRoute,
+  constructor(private route: ActivatedRoute,
+    private router: Router,
     private flightService: FlightService) { }
 
   flightId: string = 'not loaded'
@@ -25,8 +26,22 @@ export class BookFlighComponent implements OnInit {
   private findFlight = (flightId: string | null) => {
     this.flightId = flightId ?? 'not passed';
 
+    this.flightService.findFlight({ id: this.flightId }).subscribe(f => this.flight = f,
+      this.handleError)
+
   }
 
+  private handleError = (err: any) => {
+
+    if (err.status == 404) {
+      alert("Flight not found!")
+      this.router.navigate(['/search-flights'])
+    }
+
+    console.log("Response Error. Status: ", err.status)
+    console.log("Response Error. Status Text: ", err.statusText)
+    console.log(err);
+  }
 
 
 }
--- Flights/Controllers/FlightController.cs
+++ Flights/Controllers/FlightController.cs
@@ -1,14 +1,10 @@
 ﻿using Flights.ReadModels;
 using Microsoft.AspNetCore.Mvc;
-using System;
 
 namespace Flights.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    //These will be added to all actions in this controller
-    [ProducesResponseType(StatusCodes.Status400BadRequest)] 
-    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     public class FlightController : ControllerBase
     {
         private readonly ILogger<FlightController> _logger;
@@ -16,83 +12,83 @@ namespace Flights.Controllers
         static Random random = new Random();
 
         static private FlightRm[] flights = new FlightRm[]
-        {
-              new (   Guid.NewGuid(),
-                    "American Airlines",
-                    random.Next(90, 5000).ToString(),
-                    new TimePlaceRm("Los Angeles",DateTime.Now.AddHours(random.Next(1, 3))),
-                    new TimePlaceRm("Istanbul",DateTime.Now.AddHours(random.Next(4, 10))),
+            {
+        new (   Guid.NewGuid(),
+                "American Airlines",
+                random.Next(90, 5000).ToString(),
+                new TimePlaceRm("Los Angeles",DateTime.Now.AddHours(random.Next(1, 3))),
+                new TimePlaceRm("Istanbul",DateTime.Now.AddHours(random.Next(4, 10))),
                     random.Next(1, 853)),
-                new (   Guid.NewGuid(),
-                        "Deutsche BA",
-                        random.Next(90, 5000).ToString(),
-                        new TimePlaceRm("Munchen",DateTime.Now.AddHours(random.Next(1, 10))),
-                        new TimePlaceRm("Schiphol",DateTime.Now.AddHours(random.Next(4, 15))),
-                        random.Next(1, 853)),
-                new (   Guid.NewGuid(),
-                        "British Airways",
-                        random.Next(90, 5000).ToString(),
-                        new TimePlaceRm("London, England",DateTime.Now.AddHours(random.Next(1, 15))),
-                        new TimePlaceRm("Vizzola-Ticino",DateTime.Now.AddHours(random.Next(4, 18))),
-                            random.Next(1, 853)),
-                new (   Guid.NewGuid(),
-                        "Basiq Air",
-                        random.Next(90, 5000).ToString(),
-                        new TimePlaceRm("Amsterdam",DateTime.Now.AddHours(random.Next(1, 21))),
-                        new TimePlaceRm("Glasgow, Scotland",DateTime.Now.AddHours(random.Next(4, 21))),
-                            random.Next(1, 853)),
-                new (   Guid.NewGuid(),
-                        "BB Heliag",
-                        random.Next(90, 5000).ToString(),
-                        new TimePlaceRm("Zurich",DateTime.Now.AddHours(random.Next(1, 23))),
-                        new TimePlaceRm("Baku",DateTime.Now.AddHours(random.Next(4, 25))),
-                            random.Next(1, 853)),
-                new (   Guid.NewGuid(),
-                        "Adria Airways",
-                        random.Next(90, 5000).ToString(),
-                        new TimePlaceRm("Ljubljana",DateTime.Now.AddHours(random.Next(1, 15))),
-                        new TimePlaceRm("Warsaw",DateTime.Now.AddHours(random.Next(4, 19))),
-                            random.Next(1, 853)),
-                new (   Guid.NewGuid(),
-                        "ABA Air",
-                        random.Next(90, 5000).ToString(),
-                        new TimePlaceRm("Praha Ruzyne",DateTime.Now.AddHours(random.Next(1, 55))),
-                        new TimePlaceRm("Paris",DateTime.Now.AddHours(random.Next(4, 58))),
-                            random.Next(1, 853)),
-                new (   Guid.NewGuid(),
-                        "AB Corporate Aviation",
-                        random.Next(90, 5000).ToString(),
-                        new TimePlaceRm("Le Bourget",DateTime.Now.AddHours(random.Next(1, 58))),
-                        new TimePlaceRm("Zagreb",DateTime.Now.AddHours(random.Next(4, 60))),
-                            random.Next(1, 853))
-
-        };
+        new (   Guid.NewGuid(),
+                "Deutsche BA",
+                random.Next(90, 5000).ToString(),
+                new TimePlaceRm("Munchen",DateTime.Now.AddHours(random.Next(1, 10))),
+                new TimePlaceRm("Schiphol",DateTime.Now.AddHours(random.Next(4, 15))),
+                random.Next(1, 853)),
+        new (   Guid.NewGuid(),
+                "British Airways",
+                random.Next(90, 5000).ToString(),
+                new TimePlaceRm("London, England",DateTime.Now.AddHours(random.Next(1, 15))),
+                new TimePlaceRm("Vizzola-Ticino",DateTime.Now.AddHours(random.Next(4, 18))),
+                    random.Next(1, 853)),
+        new (   Guid.NewGuid(),
+                "Basiq Air",
+                random.Next(90, 5000).ToString(),
+                new TimePlaceRm("Amsterdam",DateTime.Now.AddHours(random.Next(1, 21))),
+                new TimePlaceRm("Glasgow, Scotland",DateTime.Now.AddHours(random.Next(4, 21))),
+                    random.Next(1, 853)),
+        new (   Guid.NewGuid(),
+                "BB Heliag",
+                random.Next(90, 5000).ToString(),
+                new TimePlaceRm("Zurich",DateTime.Now.AddHours(random.Next(1, 23))),
+                new TimePlaceRm("Baku",DateTime.Now.AddHours(random.Next(4, 25))),
+                    random.Next(1, 853)),
+        new (   Guid.NewGuid(),
+                "Adria Airways",
+                random.Next(90, 5000).ToString(),
+                new TimePlaceRm("Ljubljana",DateTime.Now.AddHours(random.Next(1, 15))),
+                new TimePlaceRm("Warsaw",DateTime.Now.AddHours(random.Next(4, 19))),
+                    random.Next(1, 853)),
+        new (   Guid.NewGuid(),
+                "ABA Air",
+                random.Next(90, 5000).ToString(),
+                new TimePlaceRm("Praha Ruzyne",DateTime.Now.AddHours(random.Next(1, 55))),
+                new TimePlaceRm("Paris",DateTime.Now.AddHours(random.Next(4, 58))),
+                    random.Next(1, 853)),
+        new (   Guid.NewGuid(),
+                "AB Corporate Aviation",
+                random.Next(90, 5000).ToString(),
+                new TimePlaceRm("Le Bourget",DateTime.Now.AddHours(random.Next(1, 58))),
+                new TimePlaceRm("Zagreb",DateTime.Now.AddHours(random.Next(4, 60))),
+                    random.Next(1, 853))
+            };
 
         public FlightController(ILogger<FlightController> logger)
         {
             _logger = logger;
         }
 
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        [ProducesResponseType(typeof(IEnumerable<FlightRm>), 200)]
         [HttpGet]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(500)]
+        [ProducesResponseType(typeof(IEnumerable<FlightRm>), 200)]
         public IEnumerable<FlightRm> Search()
-            => flights;
+        => flights;
 
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [HttpGet("{id}")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(500)]
+        [ProducesResponseType(typeof(FlightRm), 200)]
         public ActionResult<FlightRm> Find(Guid id)
-        { 
+        {
             var flight = flights.SingleOrDefault(f => f.Id == id);
 
             if (flight == null)
                 return NotFound();
 
             return Ok(flight);
-
         }
+
     }
 }
